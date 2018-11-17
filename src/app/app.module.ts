a4f02843060d717ed7f274e6c0bc2b66240e733d
Will <doggy.huang@gmail.com>
2018-11-17 15:01:35 +0800

    建立 FooterInfo 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterInfoComponent } from './footer-info/footer-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterInfoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/footer-info/footer-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-info',
+  templateUrl: './footer-info.component.html',
+  styleUrls: ['./footer-info.component.css']
+})
+export class FooterInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
